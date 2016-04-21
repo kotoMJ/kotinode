81f1d2d2bf77d1f24ef3e9279f1337c3934a91de
Michal Jenicek <michal.jenicek@strv.com>
2016-04-21 06:53:27 +0200

    local mongo start script

---
+++ start.mongo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo ">> use [mongo] command to access database (use: show dbs, use kotinode, ...)"
+echo ">> use [mongod] command to start database process"
+export NODE_ENV=dev
+mongod
