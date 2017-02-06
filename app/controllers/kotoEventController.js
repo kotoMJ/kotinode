14f8b4288cf8ef764eb808978b190095518dfa2b
Michal Jenicek <michal.jenicek@strv.com>
2017-02-06 16:02:15 +0100

    Add api_key check for PUT/update route.

--- app/controllers/kotoEventController.js
+++ app/controllers/kotoEventController.js
@@ -76,18 +76,24 @@ exports.putEvenet = function (req, res) {
     //
     //     logger.log(req, 'found:'+kotoEventBatch);
     // });
-
-    var payload = JSON.parse(JSON.stringify(req.body));
-    payload.date = moment(payload.date, "YYYY-MM-DDTHH:mm:ss.sssZ").toDate();
-    KotoEventModel.update({_id: req.params.batch_id}, {$push: {eventList: payload}}, {upsert: false}, function (err, raw) {
-        if (err) {
-            logger.log(req, 'Error!');
-            res.json({message: err});
-        } else {
-            logger.log(req, 'Noerror!');
-            res.json({message: raw});
-        }
-    });
+    var apiKey = req.headers['apikey'];
+    if (apiKey === undefined) {
+        res.status(401).json({"message": "Missing or incomplete authentication parameters"})
+    } else if (kotiConfig.api_key === apiKey) {
+        var payload = JSON.parse(JSON.stringify(req.body));
+        payload.date = moment(payload.date, "YYYY-MM-DDTHH:mm:ss.sssZ").toDate();
+        KotoEventModel.update({_id: req.params.batch_id}, {$push: {eventList: payload}}, {upsert: false}, function (err, raw) {
+            if (err) {
+                logger.log(req, 'Error!');
+                res.json({message: err});
+            } else {
+                logger.log(req, 'Noerror!');
+                res.status(302).json({message: raw});
+            }
+        });
+    } else {
+        res.status(403).json({"message": "Missing permissions!"})
+    }
 
 };
 
