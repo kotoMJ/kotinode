f2b6ea5c60b08bb3891ad9211adc8a3afcf7d655
Michal Jenicek <michal.jenicek@strv.com>
2017-02-10 08:51:11 +0100

    Change source for PURGE events to json with empty eventList (As pre-generated events doesn't have _id's and is not possible to delete them). Add web feature ability to delete events from bundle on click.

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -20,7 +20,7 @@ exports.getApiRouter = function () {
         logger.log(req, 'rid test');
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Credentials", "true");
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        res.header("Access-Control-Allow-Methods", "DELETE,GET,HEAD,OPTIONS,POST,PUT");
         res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept, ApiKey");
 
         //log basic incomming params
