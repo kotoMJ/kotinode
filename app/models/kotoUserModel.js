4cbf840680b1d6b94bf9eb5f2ca1549b113675d3
Michal Jenicek <michal.jenicek@strv.com>
2017-02-17 09:19:29 +0100

    Add country code to phone.

--- app/models/kotoUserModel.js
+++ app/models/kotoUserModel.js
@@ -5,7 +5,11 @@ var UserItemSchema = new Schema({
     role: {type: String, required: true},
     name: [{value: {type: String, required: true}, primary: {type: Boolean, required: true}}],
     surname: {type: String, required: true},
-    phone: [{value: {type: Number, required: false}, type: {type: String, required: true}}],
+    phone: [{
+        value: {type: Number, required: false},
+        type: {type: String, required: true},
+        countryCode: {type: Number, required: true}
+    }],
     email: [{value: {type: String, required: false}, type: {type: String, required: true}}],
     address: [{
         municipality: {type: String, required: true},
