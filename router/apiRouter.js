9ca0be7e7573de8d6f0e69cb082489767b03a3e6
Michal Jenicek <michal.jenicek@strv.com>
2017-03-27 17:29:31 +0200

    Add apitoken access control header.

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -23,7 +23,7 @@ exports.getApiRouter = function () {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Credentials", "true");
         res.header("Access-Control-Allow-Methods", "DELETE,GET,HEAD,OPTIONS,POST,PUT");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept, ApiToken");
+        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept, ApiToken, apitoken");
 
         //log basic incomming params
         logger.log(req, 'params:' + JSON.stringify(req.params));
