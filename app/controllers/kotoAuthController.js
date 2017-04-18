46df58fed35ca85d51ce8bcec49e7909e15ac877
Michal Jenicek <michal.jenicek@strv.com>
2017-04-18 07:31:34 +0200

    Extract auth method to authController.

--- app/controllers/kotoAuthController.js
+++ app/controllers/kotoAuthController.js
@@ -87,3 +87,32 @@ exports.verifyToken = function (req, res, tokenVerifiedCallback) {
         res.status(403).json({ "message": "Unexpected authentization error!" })
     }
 }
+
+exports.verifyGQLTokenPromise = function (requestId, apiToken) {
+    return new Promise(function (resolve, reject) {
+        if (apiToken === undefined) {
+            reject("Missing or incomplete authentication parameters")
+        } else {
+            logger.log(requestId, apiToken)
+            jwt.verify(apiToken, kotiConfig.api_key, function (err, decoded) {
+                if (decoded) {
+                    const now = new Date().getTime() / 1000;
+                    if (now > apiToken.exp) {
+                        logger.log(requestId, 'Expired token for USER:' + decoded.user + ' ROLE:' + decoded.role)
+                        reject("Authentication parameters expired!")
+                    } else {
+                        logger.log(requestId, 'Valid token for USER:' + decoded.user + ' ROLE:' + decoded.role)
+                        resolve(decoded)
+                    }
+                } else {
+                    logger.err(requestId, 'verifyToken.verify failed:' + err)
+                    if (err && err.name === 'TokenExpiredError') {
+                        reject("Authentication parameters expired!")
+                    } else {
+                        reject("Missing permissions!")
+                    }
+                }
+            });
+        }
+    })
+}
--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -1,5 +1,6 @@
 var KotoEventModel = require('../models/kotoEventModel');
 var KotoNotifyModel = require('../models/kotoNotifyModel');
+var KotoAuthController = require('../controllers/kotoAuthController')
 const constants = require('../utils/const')
 const Promise = require('bluebird');
 var jwt = require('jsonwebtoken');
@@ -14,7 +15,7 @@ exports.resolvers = {
         },
         notification(root, args, context){
             try {
-                return verifyGQLTokenPromise(context.requestId, context.apiToken)
+                return KotoAuthController.verifyGQLTokenPromise(context.requestId, context.apiToken)
                     .then(() => KotoNotifyModel.find().sort({ messageArriveDateTime: constants.DESC_SORT_ORDER }).exec())
             } catch (err) {
                 return err
@@ -22,33 +23,4 @@ exports.resolvers = {
             }
         }
     }
-}
-
-const verifyGQLTokenPromise = function (requestId, apiToken) {
-    return new Promise(function (resolve, reject) {
-        if (apiToken === undefined) {
-            reject("Missing or incomplete authentication parameters")
-        } else {
-            logger.log(requestId, apiToken)
-            jwt.verify(apiToken, kotiConfig.api_key, function (err, decoded) {
-                if (decoded) {
-                    const now = new Date().getTime() / 1000;
-                    if (now > apiToken.exp) {
-                        logger.log(requestId, 'Expired token for USER:' + decoded.user + ' ROLE:' + decoded.role)
-                        reject("Authentication parameters expired!")
-                    } else {
-                        logger.log(requestId, 'Valid token for USER:' + decoded.user + ' ROLE:' + decoded.role)
-                        resolve(decoded)
-                    }
-                } else {
-                    logger.err(requestId, 'verifyToken.verify failed:' + err)
-                    if (err && err.name === 'TokenExpiredError') {
-                        reject("Authentication parameters expired!")
-                    } else {
-                        reject("Missing permissions!")
-                    }
-                }
-            });
-        }
-    })
 }
